6ee561deb783b57b359f274f2285d8d3e5859344
Giovanni Luca Cusano <giovannilucacusano@gmail.com>
2026-07-19 22:51:15 +0200

    rimuove capacitor: non usato dal progetto

--- client/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Divide_et_Quantifica',
-  webDir: 'www'
-};
-
-export default config;
